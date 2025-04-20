a5a46ef6861e963646caa8a9ad1afaeb87da77f5
Diego Ferreira <diego19912011@live.com>
2025-04-20 14:20:15 -0300

    Continuação projeto inicial springboot

--- primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -7,8 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
+		/*
+		Modo convencional de instanciar objetos
+
+		Calculadora calculadora = new Calculadora();
+		System.out.println("Resultado da soma: " + calculadora.somar(5, 6));
+		*/
 	}
 
 }
