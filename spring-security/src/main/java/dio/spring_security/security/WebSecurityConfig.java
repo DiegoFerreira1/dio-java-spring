349cca29af53b0678520ef8718dd2246e897f0e0
Diego Ferreira <diego19912011@live.com>
2025-05-07 13:20:48 -0300

    COntinuação Spring Security

--- spring-security/src/main/java/dio/spring_security/security/WebSecurityConfig.java
+++ spring-security/src/main/java/dio/spring_security/security/WebSecurityConfig.java
@@ -1,13 +1,19 @@
 package dio.spring_security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import dio.spring_security.config.SecurityDatabaseService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -43,9 +49,15 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").permitAll()
+                .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                .requestMatchers("/admin/**").hasRole("MANAGERS")
+                .requestMatchers("/user/**").hasRole("USERS")
                 .anyRequest().authenticated()
             )
-            .formLogin(); // Exibe formulário de login ao invés de popup
+            .formLogin()
+            .and()
+            .httpBasic(); // Opcional
     
         return http.build();
     }
